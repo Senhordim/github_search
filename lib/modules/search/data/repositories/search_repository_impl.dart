d6281b7688d6621acf73da94a2af362326237be7
Diego Collares <diego.collares@gmail.com>
2024-01-18 21:14:59 -0300

    create repository and test structure of data clean

---
+++ lib/modules/search/data/repositories/search_repository_impl.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+
+import '../../domain/entities/result_search.dart';
+import '../../domain/errors/errors.dart';
+import '../../domain/repositories/search_repository.dart';
+import '../data_source/search_data_source.dart';
+
+class SearchRepositoryImpl implements SearchRepository {
+  final SearchDataSource dataSource;
+
+  SearchRepositoryImpl({required this.dataSource});
+
+  @override
+  Future<Either<FailureSearch, List<ResultSearch>?>> search(
+      String searchText) async {
+    final result = await dataSource.getSearch(searchText);
+    return Right(result);
+  }
+}
---
+++ test/modules/search/data/repositories/search_repository_impl_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:github_search/modules/search/data/data_source/search_data_source.dart';
+import 'package:github_search/modules/search/data/models/result_search_model.dart';
+import 'package:github_search/modules/search/data/repositories/search_repository_impl.dart';
+import 'package:github_search/modules/search/domain/entities/result_search.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockSearchDataSource extends Mock implements SearchDataSource {}
+
+void main() {
+  final dataSource = MockSearchDataSource();
+  final repository = SearchRepositoryImpl(
+    dataSource: dataSource,
+  );
+
+  test(
+    'Deve retornar uma lista de ResultSearch',
+    () async {
+      when(() => dataSource.getSearch('senhordim'))
+          .thenAnswer((_) async => <ResultSearchModel>[]);
+      final result = await repository.search('senhordim');
+      expect(result | null, isA<List<ResultSearch>>());
+    },
+  );
+}
